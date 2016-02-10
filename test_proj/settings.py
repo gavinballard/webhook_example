fe86331710bd1e1b173c70ea818968adae75d302
Gavin Ballard <gavin@gavinballard.com>
2016-02-10 14:59:38 +0100

    Add webhooks app and configuration.

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'shopify_webhook',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
--- test_proj/urls.py
+++ test_proj/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shopify_webhook.views import WebhookView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^order/', WebhookView.as_view(), name = 'order'),
 ]
